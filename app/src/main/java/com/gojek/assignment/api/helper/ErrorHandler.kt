e70a7184d13d87d1211fa32d2f4f37b068d27797
Vijay Makwana <makwanavijay153@gmail.com>
2019-12-22 03:59:12 +0530

    feat: add ErrorHandler for manage api and network error

---
+++ app/src/main/java/com/gojek/assignment/api/helper/ErrorHandler.kt
@@ -0,0 +1,30 @@
+package com.gojek.assignment.api.helper
+
+import android.content.Context
+import androidx.appcompat.app.AlertDialog
+import com.gojek.assignment.utils.isNetworkAvailable
+
+
+interface ErrorHandler {
+    fun onError(resource: Result<*>)
+}
+
+class AlertErrorHandler(
+    private val context: Context,
+    private val isCancelable: Boolean = true,
+    private var isNetworkAvailable: ((isNetworkAvailable: Boolean) -> Unit)
+) : ErrorHandler {
+
+    override fun onError(resource: Result<*>) {
+        if (context.isNetworkAvailable()) {
+            val builder = AlertDialog.Builder(context)
+            builder.setCancelable(isCancelable)
+            builder.setPositiveButton(android.R.string.yes) { dialogInterface, _ ->
+                dialogInterface.dismiss()
+            }
+            builder.show()
+        } else {
+            isNetworkAvailable.invoke(false)
+        }
+    }
+}
