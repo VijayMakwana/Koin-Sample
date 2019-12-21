092cf1954b11c655ced18439ed298689c2d82a3d
Vijay Makwana <makwanavijay153@gmail.com>
2019-12-21 19:55:04 +0530

    feat: add view visibility extensions

---
+++ app/src/main/java/com/gojek/assignment/utils/Extensions.kt
@@ -0,0 +1,24 @@
+package com.gojek.assignment.utils
+
+import android.view.View
+
+/**
+ * Sets the view's visibility to GONE
+ */
+fun View.hide() {
+    visibility = View.GONE
+}
+
+/**
+ * Sets the view's visibility to VISIBLE
+ */
+fun View.show() {
+    visibility = View.VISIBLE
+}
+
+/**
+ * Sets the view's visibility to INVISIBLE
+ */
+fun View.invisible() {
+    visibility = View.INVISIBLE
+}
