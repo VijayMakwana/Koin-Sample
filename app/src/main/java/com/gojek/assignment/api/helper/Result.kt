e4869bde925c09bfb05b936432f470f9d9e420b9
Vijay Makwana <makwanavijay153@gmail.com>
2019-12-22 03:06:51 +0530

    feat: add Result class for manage data api state

---
+++ app/src/main/java/com/gojek/assignment/api/helper/Result.kt
@@ -0,0 +1,20 @@
+package com.gojek.assignment.api.helper
+
+class Result<T> private constructor(
+    var state: State = State.NONE,
+    var data: T? = null,
+    var errorMessage: String? = null
+) {
+    companion object {
+        fun <T> loading(data: T? = null) =
+            Result<T>(state = State.LOADING, data = data)
+
+        fun <T> success(data: T?) =
+            Result<T>(state = State.SUCCESS, data = data)
+
+        fun <T> error(data: T? = null, errorMessage: String?) =
+            Result(state = State.ERROR, data = data, errorMessage = errorMessage)
+    }
+
+    enum class State { NONE, LOADING, SUCCESS, ERROR }
+}
