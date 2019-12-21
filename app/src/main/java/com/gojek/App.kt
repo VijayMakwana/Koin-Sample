e4a152ad8876e893998d688dac9ca08008dc279b
Vijay Makwana <makwanavijay153@gmail.com>
2019-12-22 03:13:21 +0530

    feat: add RepoRepository and TrendingViewModel in koin di

--- app/src/main/java/com/gojek/App.kt
+++ app/src/main/java/com/gojek/App.kt
@@ -3,8 +3,12 @@ package com.gojek
 import android.app.Application
 import com.gojek.assignment.api.di.NetworkModule
 import com.gojek.assignment.api.di.NetworkModuleImpl
+import com.gojek.assignment.api.di.RepoRepository
+import com.gojek.assignment.api.di.RepoRepositoryImpl
+import com.gojek.assignment.ui.repositories.trending.TrendingRepoViewModel
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.context.startKoin
 import org.koin.dsl.module
 
@@ -16,6 +20,10 @@ class App : Application() {
         // all the app functionality related modules here
         val appModule = module {
             single<NetworkModule> { NetworkModuleImpl() }
+
+            single<RepoRepository> { RepoRepositoryImpl(get()) }
+
+            viewModel { TrendingRepoViewModel(get()) }
         }
 
         // start dependency injection with koin
