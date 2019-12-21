1d1ab83b4a98b66dc3920656ec1cea4a25dce146
Vijay Makwana <makwanavijay153@gmail.com>
2019-12-22 03:09:22 +0530

    feat: add RepoModel and API Request

--- app/src/main/java/com/gojek/assignment/api/ApiInterface.kt
+++ app/src/main/java/com/gojek/assignment/api/ApiInterface.kt
@@ -1,4 +1,10 @@
 package com.gojek.assignment.api
 
+import com.gojek.assignment.api.model.RepoModel
+import retrofit2.Call
+import retrofit2.http.GET
+
 interface ApiInterface {
+    @GET("repositories")
+    fun getRepos(): Call<List<RepoModel>>
 }
