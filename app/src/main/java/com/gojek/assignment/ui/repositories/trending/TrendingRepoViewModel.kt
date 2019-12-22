1aa17398013f2c03d226540f46765129fc2f540d
Vijay Makwana <makwanavijay153@gmail.com>
2019-12-22 13:56:56 +0530

    feat: add fetchRepository function for get repolist

--- app/src/main/java/com/gojek/assignment/ui/repositories/trending/TrendingRepoViewModel.kt
+++ app/src/main/java/com/gojek/assignment/ui/repositories/trending/TrendingRepoViewModel.kt
@@ -1,24 +1,30 @@
 package com.gojek.assignment.ui.repositories.trending
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.asLiveData
-import androidx.lifecycle.liveData
+import androidx.lifecycle.*
 import com.gojek.assignment.api.di.RepoRepository
 import com.gojek.assignment.api.helper.Result
 import com.gojek.assignment.api.model.RepoModel
 import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.launch
 
 class TrendingRepoViewModel(private val repoRepository: RepoRepository) : ViewModel() {
 
-    val data: LiveData<Result<List<RepoModel>>> =
-        liveData {
-            emitSource(
-                repoRepository.getTrendingRepos()
-                    .onStart {
-                        emit(Result.loading())
-                    }.asLiveData()
-            )
-        }
+    private val mReposResult = MutableLiveData<Result<List<RepoModel>>>()
+    val reposResult: LiveData<Result<List<RepoModel>>> = mReposResult
+
+    val repoList = Transformations.map(reposResult) {
+        it.data.orEmpty()
+    }
+
+    init {
+        fetchTrendingRepos() // call initially
+    }
+
+    fun fetchTrendingRepos() = viewModelScope.launch {
+        repoRepository.getTrendingRepos()
+            .onStart {
+                emit(Result.loading())
+            }.asLiveData().observeForever { mReposResult.value = it }
+    }
 
 }
