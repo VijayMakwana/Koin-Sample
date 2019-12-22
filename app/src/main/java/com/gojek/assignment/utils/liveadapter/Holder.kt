d75f9091b49ce6ea3f6d14fbe8566478ba43e8cd
Vijay Makwana <makwanavijay153@gmail.com>
2019-12-22 15:06:12 +0530

    feat: add liveAdapter for easily implement recycler-view adapter

---
+++ app/src/main/java/com/gojek/assignment/utils/liveadapter/Holder.kt
@@ -0,0 +1,9 @@
+package com.gojek.assignment.utils.liveadapter
+
+import androidx.databinding.ViewDataBinding
+import androidx.recyclerview.widget.RecyclerView
+
+open class Holder<B : ViewDataBinding>(val binding: B) : RecyclerView.ViewHolder(binding.root) {
+    internal var created = false
+}
+
