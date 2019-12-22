806b44a2bcad1c6395b89e7a328db3eb30807dd0
Vijay Makwana <makwanavijay153@gmail.com>
2019-12-22 15:01:01 +0530

    feat: add circle image and colorFilter functions

--- app/src/main/java/com/gojek/assignment/utils/BindingAdapters.kt
+++ app/src/main/java/com/gojek/assignment/utils/BindingAdapters.kt
@@ -1,9 +1,15 @@
 package com.gojek.assignment.utils
 
+import android.graphics.Color
+import android.graphics.PorterDuff
 import android.view.View
+import android.widget.ImageView
 import androidx.databinding.BindingAdapter
+import com.bumptech.glide.request.RequestOptions
+import com.gojek.assignment.R
 import com.gojek.assignment.api.helper.ErrorHandler
 import com.gojek.assignment.api.helper.Result
+import com.gojek.assignment.utils.glide.GlideApp
 
 object BindingAdapters {
     @JvmStatic
@@ -28,4 +34,30 @@ object BindingAdapters {
             }
         }
     }
+
+    /**
+     * Loads the image into ImageView from the URL
+     */
+    @JvmStatic
+    @BindingAdapter("circleImageUrl")
+    fun setCircleImage(imageView: ImageView, url: String?) {
+        GlideApp.with(imageView)
+            .load(url)
+            .apply(RequestOptions().circleCrop())
+            .placeholder(R.drawable.bg_placeholder).into(imageView)
+
+    }
+
+    /**
+     * Applies the custom tint to imageview
+     */
+    @JvmStatic
+    @BindingAdapter("colorFilter")
+    fun colorFilter(imageView: ImageView, color: String?) {
+        color?.let {
+            imageView.setColorFilter(Color.parseColor(it), PorterDuff.Mode.SRC_ATOP)
+        }
+    }
+
+
 }
